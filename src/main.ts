e7f93bb84f8342600ae68a6007bcac2e6153a3ba
odei mzabi <odeimzabi2000@gmail.com>
2021-02-03 07:27:47 -0800

    first deploy

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser())
-  app.enableCors({credentials:true})
+  app.enableCors({credentials:true,origin:"*"})
   await app.listen(process.env.PORT||5000);
 }
 bootstrap();
