9db8786b8eabb1bebfa3fc092b8b833ea41f7898
odei mzabi <odeimzabi2000@gmail.com>
2021-03-01 07:24:26 -0800

    first commit

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
   app.enableCors({ origin: true,
             methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
             credentials: true,})
-  await app.listen(process.env.PORT||5000);
+  await app.listen(process.env.PORT||4000);
 }
 bootstrap();
